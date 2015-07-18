b5421f18c61742a00cc380f0bd6890f483bbaf28
Larry Gilbert <larry@l2g.to>
2015-07-18 14:37:00 -0700

    perforce 2015.1.1126382
    * Update to match current upstream version
    * Add test that verifies executable's version/build

--- perforce.rb
+++ perforce.rb
@@ -1,6 +1,6 @@
 class Perforce < Formula
   homepage "http://www.perforce.com/"
-  version "2015.1.1024208"
+  version "2015.1.1126382"
 
   if MacOS.prefer_64_bit?
     url "http://filehost.perforce.com/perforce/r15.1/bin.macosx105x86_64/p4"
@@ -12,5 +12,14 @@ class Perforce < Formula
 
   def install
     bin.install "p4"
+  end
+
+  test do
+    os_tag = (MacOS.prefer_64_bit? ? "P4/MACOSX105X86_64" : "P4/MACOSX105X86")
+    (version_year, version_minor, version_build) = version.to_s.split(".")
+    assert_match(
+      %r[#{os_tag}/#{version_year}\.#{version_minor}/#{version_build} ],
+      shell_output("#{bin}/p4 -V")
+    )
   end
 end
